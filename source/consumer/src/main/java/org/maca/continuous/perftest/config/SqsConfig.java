29f1b5268046fed9958865c81de1a24cb8b52591
tonchan <cleartone1216@gmail.com>
2021-11-20 22:31:35 +0900

    add sqs

--- source/consumer/src/main/java/org/maca/continuous/perftest/config/SqsConfig.java
+++ source/consumer/src/main/java/org/maca/continuous/perftest/config/SqsConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@ComponentScan("org.maca.consumer.app.listener")
+@ComponentScan("org.maca.continuous.perftest.app.listener")
 @Configuration
 public class SqsConfig {
 
