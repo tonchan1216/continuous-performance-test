5c677ca9870256703e67c9c4cd5e80cba5dc5e7b
tonchan <cleartone1216@gmail.com>
2021-12-09 09:01:16 +0900

    add params for gatling

--- load-test/scripts/gatling/sample.scala
+++ load-test/scripts/gatling/sample.scala
@@ -11,17 +11,14 @@ class BasicSimulation extends Simulation {
   val t_rampUp = Integer.getInteger("ramp-up", 1).toInt
   val t_holdFor = Integer.getInteger("hold-for", 60).toInt
   val t_throughput = Integer.getInteger("throughput", 100).toInt
+  val cluster_size = Integer.getInteger("CLUSTER_SIZE", 0).toInt
+  val partition_id = Integer.getInteger("PARTITION_ID", 0).toInt
+
   val httpConf = http.baseUrl("http://blazedemo.com/")
 
   // 'forever' means each thread will execute scenario until
   // duration limit is reached
   val loopScenario = scenario("Loop Scenario").forever() {
-    exec(http("index").get("/"))
-  }
-
-  // if you want to set an iteration limit (instead of using duration limit),
-  // you can use the following scenario
-  val iterationScenario = scenario("Iteration Scenario").repeat(t_iterations) {
     exec(http("index").get("/"))
   }
 
